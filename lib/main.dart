86e571173d1199cc9cb996e4cb9caf2aed069822
najat1elaissaoui <najatelaissaoui347@gmail.com>
2025-03-10 13:26:14 +0100

    login/firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:emotion_music_player/pages/bottomnav.dart';
 import 'package:emotion_music_player/pages/chat.dart';
+import 'package:emotion_music_player/pages/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -33,7 +37,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:BottomNav()
+      home:LoginScreen()
     );
   }
 }
---
+++ lib/pages/login.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import '../Services/authentication.dart';
+import '../Widget/snackbar.dart';
+import '../Widget/text_field.dart';
+import '../Widget/button.dart'; // Assurez-vous que ce fichier est bien importé
+import 'signup.dart';
+import 'bottomnav.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool isLoading = false;
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  // Authentification de l'utilisateur
+  void loginUser() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    String res = await AuthMethod().loginUser(
+      email: emailController.text.trim(),
+      password: passwordController.text.trim(),
+    );
+
+    setState(() {
+      isLoading = false;
+    });
+
+    if (res == "success") {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const BottomNav()),
+      );
+    } else {
+      showSnackBar(context, res);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: height / 3,
+                  child: Image.asset('images/login.jpg'),
+                ),
+                const SizedBox(height: 20),
+                TextFieldInput(
+                  icon: Icons.person,
+                  textEditingController: emailController,
+                  hintText: 'Enter your email',
+                  textInputType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 15),
+                TextFieldInput(
+                  icon: Icons.lock,
+                  textEditingController: passwordController,
+                  hintText: 'Enter your password',
+                  textInputType: TextInputType.text,
+                  isPass: true,
+                ),
+                const SizedBox(height: 20),
+                isLoading
+                    ? const CircularProgressIndicator()
+                    : MyButtons(onTap: loginUser, text: "Log In"),
+                const SizedBox(height: 15),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have an account? "),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                              builder: (context) => const SignupScreen()),
+                        );
+                      },
+                      child: const Text(
+                        "Sign Up",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.blue),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
